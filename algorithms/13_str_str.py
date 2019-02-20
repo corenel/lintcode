db06d2fb62c6c311cceea0c61d57e2a5ef9194cd
corenel <xxdsox@gmail.com>
2019-02-20 23:40:23 +0800

    Move solutions from leetcode

---
+++ algorithms/13_str_str.py
@@ -0,0 +1,152 @@
+"""
+StrStr
+---
+
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack,
+or -1 if needle is not part of haystack.
+
+Example 1:
+    Input: haystack = "hello", needle = "ll"
+    Output: 2
+
+Example 2:
+    Input: haystack = "aaaaa", needle = "bba"
+    Output: -1
+
+Clarification:
+    What should we return when needle is an empty string? This is a great question
+    to ask during an interview.
+
+    For the purpose of this problem, we will return 0 when needle is an empty
+    string. This is consistent to C's strstr() and Java's indexOf().
+
+Reference:
+- https://algorithm.yuanbin.me/zh-hans/string/strstr.html
+- https://leetcode.com/problems/implement-strstr/
+- https://www.lintcode.com/problem/implement-strstr
+- https://www.geeksforgeeks.org/naive-algorithm-for-pattern-searching/
+- https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+"""
+
+import unittest
+
+
+def str_str_naive(haystack, needle):
+    """
+    Solution for strStr() in naive algorithm
+
+    Worst case complexity: O(m(n-m+1))
+
+    :param haystack: source string (length n) to be searched in
+    :type haystack: str
+    :param needle: target string (length m) to search
+    :type needle: str
+    :return: index of occurrence
+    :rtype: int
+    """
+    # basic case
+    if len(needle) == 0:
+        return 0
+    if len(haystack) == 0 or len(haystack) < len(needle):
+        return -1
+
+    # double for-loop to slide pattern one by one
+    for i in range(len(haystack) - len(needle) + 1):
+        # For current index i, check for pattern match
+        for j in range(len(needle)):
+            if haystack[i + j] != needle[j]:
+                break
+        else:
+            return i
+
+    return -1
+
+
+def str_str_kmp(haystack, needle):
+    """
+    Solution for strStr() in KMP (Knuth Morris Pratt) way.
+
+    The KMP matching algorithm uses degenerating property (pattern having
+    same sub-patterns appearing more than once in the pattern) of the pattern
+    and improves the worst case complexity to O(n). The basic idea behind
+    KMP’s algorithm is: whenever we detect a mismatch (after some matches),
+    we already know some of the characters in the text of the next window.
+
+    Worst case complexity: O(n)
+
+    :param haystack: source string to be searched in
+    :type haystack: str
+    :param needle: target string to search
+    :type needle: str
+    :return: index of occurrence
+    :rtype: int
+    """
+    # basic case
+    if len(needle) == 0:
+        return 0
+    if len(haystack) == 0 or len(haystack) < len(needle):
+        return -1
+
+    # preprocess the pattern
+    # create lps[] that will hold the longest prefix suffix
+    # values for pattern
+    lps = [0 for _ in range(len(needle))]
+    length = 0
+    i = 1
+    # the loop calculates lps[i] for i = 1 to M-1
+    while i < len(needle):
+        if needle[i] == needle[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            # this is tricky. Consider the example.
+            # AAACAAAA and i = 7. The idea is similar
+            # to search step.
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+
+    # start searching
+    i = j = 0
+    # indices = []
+    while i < len(haystack):
+        if haystack[i] == needle[j]:
+            i += 1
+            j += 1
+        if j == len(needle):
+            # indices.append(i)
+            # j = lps[j - 1]
+            return i - j
+        # mismatch after j matches
+        elif i < len(haystack) and haystack[i] != needle[j]:
+            # do not match lps[0..lps[j-1]] characters,
+            # they will match anyway
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+
+    return -1
+
+
+class TestStrStr(unittest.TestCase):
+    def test_str_str_naive(self):
+        self.assertEqual(2, str_str_naive('hello', 'll'))
+        self.assertEqual(-1, str_str_naive('aaaaa', 'bba'))
+        self.assertEqual(0, str_str_naive('hello', ''))
+        self.assertEqual(-1, str_str_naive('', 'hello'))
+
+    def test_str_str_kmp(self):
+        self.assertEqual(2, str_str_kmp('hello', 'll'))
+        self.assertEqual(-1, str_str_kmp('aaaaa', 'bba'))
+        self.assertEqual(0, str_str_kmp('hello', ''))
+        self.assertEqual(-1, str_str_kmp('', 'hello'))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ algorithms/158_valid_anagram.py
@@ -0,0 +1,136 @@
+"""
+Valid Anagram
+---
+
+Given two strings s and t , write a function to determine if t is an anagram
+of s.
+
+Example 1:
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+
+Example 2:
+    Input: s = "rat", t = "car"
+    Output: false
+    Clarification
+
+Note:
+- Two strings are anagram if they can be the same after change the order of
+  characters.
+- You may assume the string contains only lowercase alphabets.
+
+Follow up:
+    What if the inputs contain unicode characters? How would you adapt your
+    solution to such case?
+
+Reference:
+- https://algorithm.yuanbin.me/zh-hans/string/two_strings_are_anagrams.html
+- https://www.geeksforgeeks.org/check-whether-two-strings-are-anagram-of-each-other/
+- https://www.lintcode.com/problem/valid-anagram
+"""
+
+import unittest
+from collections import Counter
+
+
+def is_anagram_counter(s, t):
+    """
+    Determine whether or not teo given strings are anagram by counter
+
+    :param s: source string
+    :type s: str
+    :param t: target string
+    :type t: str
+    :return: whether or not teo given strings are anagram
+    :rtype: bool
+    """
+    return len(s) == len(t) and Counter(s) == Counter(t)
+
+
+def is_anagram_hashmap(s, t):
+    """
+    Determine whether or not teo given strings are anagram by counting
+    characters
+
+    Time Complexity: O(n)
+
+    :param s: source string
+    :type s: str
+    :param t: target string
+    :type t: str
+    :return: whether or not teo given strings are anagram
+    :rtype: bool
+    """
+    # basic case
+    if len(s) != len(t):
+        return False
+
+    # construct counter
+    counter = [0 for _ in range(256)]
+    for i in range(len(s)):
+        counter[ord(s[i])] += 1
+        counter[ord(t[i])] -= 1
+
+    # cost more time (maybe ord())
+    # if len(s) >= 256:
+    #     for i in range(len(counter)):
+    #         if counter[i] != 0:
+    #             return False
+    # else:
+    #     for i in range(len(s)):
+    #         if counter[ord(s[i])] != 0:
+    #             return False
+
+    # compare counter
+    for i in range(len(counter)):
+        if counter[i] != 0:
+            return False
+
+    return True
+
+
+def is_anagram_sort(s, t):
+    """
+    Determine whether or not teo given strings are anagram by sorting
+
+    Time Complexity: O(n log n)
+
+    :param s: source string
+    :type s: str
+    :param t: target string
+    :type t: str
+    :return: whether or not teo given strings are anagram
+    :rtype: bool
+    """
+    # basic case
+    if len(s) != len(t):
+        return False
+
+    # sort strings
+    s_sorted = sorted(s)
+    t_sorted = sorted(t)
+
+    # check equality
+    for i in range(len(s_sorted)):
+        if s_sorted[i] != t_sorted[i]:
+            return False
+
+    return True
+
+
+class TestValidAnagram(unittest.TestCase):
+    def test_valid_anagram_counter(self):
+        self.assertTrue(is_anagram_counter('anagram', 'nagaram'))
+        self.assertFalse(is_anagram_counter('rat', 'car'))
+
+    def test_valid_anagram_hashmap(self):
+        self.assertTrue(is_anagram_hashmap('anagram', 'nagaram'))
+        self.assertFalse(is_anagram_hashmap('rat', 'car'))
+
+    def test_valid_anagram_sort(self):
+        self.assertTrue(is_anagram_sort('anagram', 'nagaram'))
+        self.assertFalse(is_anagram_sort('rat', 'car'))
+
+
+if __name__ == '__main__':
+    unittest.main()
