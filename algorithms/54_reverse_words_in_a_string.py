6b8e8537ff9df32258aaca7dbebe1e4a86056db4
corenel <xxdsox@gmail.com>
2019-02-21 12:46:39 +0800

    Add 54_reverse_words_in_a_string

---
+++ algorithms/54_reverse_words_in_a_string.py
@@ -0,0 +1,59 @@
+"""
+Reverse Words in a String
+-------------------------
+
+Given an input string, reverse the string word by word.
+
+Example:
+    - Input: "the sky is blue",
+    - Output: "blue is sky the".
+
+Note:
+    - A word is defined as a sequence of non-space characters.
+    - Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+    - You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+Follow up:
+    For C programmers, try to solve it in-place in O(1) space.
+
+Reference:
+    - https://algorithm.yuanbin.me/zh-hans/string/reverse_words_in_a_string.html
+    - https://leetcode.com/problems/reverse-words-in-a-string/
+    - https://www.lintcode.com/problem/reverse-words-in-a-string/description
+"""
+
+import unittest
+
+
+def reverse_words_in_a_string(string):
+    """
+    Given an input string, reverse the string word by word.
+
+    :param string: given string
+    :type string: str
+    :return: reversed string
+    :rtype: str
+    """
+    splited = [word for word in string.split(' ')
+               if word != '']
+    return ' '.join(splited[::-1])
+
+
+class TestReverseWordsInAString(unittest.TestCase):
+    def test_reverse_words_in_a_string(self):
+        self.assertEqual(
+            'blue is sky the',
+            reverse_words_in_a_string('the sky is blue'))
+        self.assertEqual(
+            'blue is sky the',
+            reverse_words_in_a_string('the  sky    is blue '))
+        self.assertEqual(
+            '',
+            reverse_words_in_a_string(' '))
+        self.assertEqual(
+            '',
+            reverse_words_in_a_string('  '))
+
+
+if __name__ == '__main__':
+    unittest.main()
